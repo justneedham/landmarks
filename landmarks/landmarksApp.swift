38b8387c45c68ffbfbfd0bb8021628e9c0836a1d
Justin Needham <justin.needham@alumnimail.pepperdine.edu>
2022-02-13 11:39:13 -0700

    initial commit

---
+++ landmarks/landmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  landmarksApp.swift
+//  landmarks
+//
+//  Created by Justin Needham on 2/13/22.
+//
+
+import SwiftUI
+
+@main
+struct landmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

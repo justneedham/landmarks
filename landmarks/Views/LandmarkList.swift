322c2dc47c43833bbe04f0284fa2b33add67a608
Justin Needham <justin.needham@alumnimail.pepperdine.edu>
2022-02-13 18:56:39 -0700

    landmarks list

---
+++ landmarks/Views/LandmarkList.swift
@@ -0,0 +1,23 @@
+//
+//  LandmarkList.swift
+//  landmarks
+//
+//  Created by Justin Needham on 2/13/22.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    var body: some View {
+        List {
+            LandmarkRow(landmark: landmarks[0])
+            LandmarkRow(landmark: landmarks[1])
+        }
+    }
+}
+
+struct LandmarkList_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkList()
+    }
+}

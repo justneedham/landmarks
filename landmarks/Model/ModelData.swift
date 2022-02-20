1e49fb0ffdba7d720ed136b8b6afb8f1f935e9c6
Justin Needham <justin.needham@alumnimail.pepperdine.edu>
2022-02-20 11:05:04 -0700

    add favorite button and persist

--- landmarks/Model/ModelData.swift
+++ landmarks/Model/ModelData.swift
@@ -7,6 +7,10 @@
 
 import Foundation
 
+final class ModelData: ObservableObject {
+    @Published var landmarks: [Landmark] = load("landmarkData.json")
+}
+
 var landmarks: [Landmark] = load("landmarkData.json")
 
 func load<T: Decodable>(_ filename: String) -> T {
---
+++ landmarks/Views/Helpers/FavoriteButton.swift
@@ -0,0 +1,28 @@
+//
+//  FavoriteButton.swift
+//  landmarks
+//
+//  Created by Justin Needham on 2/20/22.
+//
+
+import SwiftUI
+
+struct FavoriteButton: View {
+    @Binding var isSet: Bool
+    
+    var body: some View {
+        Button {
+            isSet.toggle()
+        } label: {
+            Label("Toggle Favorite", systemImage: isSet ? "star.fill" : "star")
+                .labelStyle(IconOnlyLabelStyle())
+                .foregroundColor(isSet ? .yellow : .gray)
+        }
+    }
+}
+
+struct FavoriteButton_Previews: PreviewProvider {
+    static var previews: some View {
+        FavoriteButton(isSet: .constant(true))
+    }
+}
